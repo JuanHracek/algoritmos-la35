ed816a0a31a57591fb7eb2c750135434cef77514
Juan Hracek <166658326+JuanHracek@users.noreply.github.com>
2024-05-10 11:51:52 +0000

    Ejercicio 4c

---
+++ ejercicio-4/ejercicio4c.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int valormax(int a, int b) {
+    return a > b ? a : b;
+
+
+   }
+   int main(int argc, char* argv[]) {
+   int a, b;
+   a = atoi(argv[1]);
+   b = atoi(argv[2]);
+
+   printf("%d\n", valormax(a, b) );
+
+   return 0;
+   }
